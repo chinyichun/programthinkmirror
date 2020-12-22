7add7db35d6127bda6e31d930711ef2091343b02
posclegom <posclegom@gmail.com>
2020-12-22 11:33:23 -0500

    144th commit

--- master7.sh
+++ master7.sh
@@ -7,7 +7,7 @@ do
  echo $p
  echo $i
  a="h1><a\x20href=\"..\/..\/forum5\/"
- b=".html\">浏览本博文的相关论坛<\/a><\/h1>\n<h1>"
+ b=".xml\">浏览本博文的相关论坛<\/a><\/h1>\n<h1>"
  c="sed -i -e 0,/h1>/{s/h1>/$a$i$b/} .$p"
  echo $c 
  $c
